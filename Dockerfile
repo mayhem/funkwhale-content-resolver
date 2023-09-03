02148c39882982509b12bfd09c8c6dcbf08f0d99
Robert Kaye <rob@musicbrainz.org>
2023-09-03 22:00:22 +0200

    Playlist importing is working!

--- Dockerfile
+++ Dockerfile
@@ -3,8 +3,9 @@ FROM ubuntu:22.04
 RUN apt update && apt install python3 python3-pip git -y
 
 WORKDIR /src
-COPY resolve.py requirements.txt /src
+COPY requirements.txt /src/
 
 RUN pip install -r requirements.txt
 
-CMD python3 /src/resolve.py
+COPY resolve.py /src/
+CMD flask --app resolve run --host=0.0.0.0 --port 5001
--- resolve.py
+++ resolve.py
@@ -1,14 +1,17 @@
 #!/usr/bin/env python3
 
+from flask import Flask, jsonify, request
+from werkzeug.exceptions import BadRequest, InternalServerError
 import psycopg2
+import psycopg2.extras
 
 from lb_content_resolver.fuzzy_index import FuzzyIndex
 
+DB_CONNECT = "postgresql://postgres@funkwhale_postgres_1:5432/postgres"
 
-DB_CONNECT = "postgresql://postgres@funkwhale-postgres-1:5432/postgres"
+app = Flask(__name__)
 
-
-def resolve_jspf_playlist(jspf_file: str):
+def build_track_index():
 
     index = FuzzyIndex("")
 
@@ -21,17 +24,64 @@ def resolve_jspf_playlist(jspf_file: str):
 
     artist_recording_data = []
     with psycopg2.connect(DB_CONNECT) as conn:    
-        with conn.cursor() as curs:
+        with conn.cursor(cursor_factory=psycopg2.extras.DictCursor) as curs:
             curs.execute(query)
 
             print(f"Fetch {curs.rowcount} tracks fetched.")
             for row in curs.fetchall():
-                artist_recording_data.append((row["recording_name"], row["artist_name"], row["id"]))
+                artist_recording_data.append((row["artist_name"], row["recording_name"], row["id"]))
 
     index.build(artist_recording_data)
-    print("Index built.")
-  
+    recording_id_index = { ar[2] : (ar[0], ar[1]) for ar in artist_recording_data }
+
+    return index, recording_id_index
+
+def lookup_tracks(index, query_data):
+    return index.search(query_data)
 
 
-if __name__ == "__main__":
-    resolve_jspf_playlist("test.jspf")
+def create_funkwhale_playlist(user_id, matched_tracks, jspf):
+
+    with psycopg2.connect(DB_CONNECT) as conn:    
+        with conn.cursor(cursor_factory=psycopg2.extras.DictCursor) as curs:
+
+            curs.execute("""INSERT INTO playlists_playlist (name, user_id, privacy_level, creation_date, modification_date)
+                                 VALUES (%s, %s, %s, now(), now())
+                              RETURNING id""", (jspf["playlist"]["title"], user_id, "instance"))
+            playlist_id = curs.fetchone()["id"]
+
+            for i, track in enumerate(matched_tracks):
+                curs.execute("""INSERT INTO playlists_playlisttrack (playlist_id, track_id, creation_date, index)
+                                     VALUES (%s, %s, now(), %s)""", (playlist_id, int(track["recording_id"]), i))
+
+            conn.commit()
+
+
+@app.route("/resolve", methods=["POST"])
+def resolve():
+    jspf = request.json
+
+    user_id = request.args.get("user_id", None)
+    if user_id is None:
+        raise BadRequest("Parameter user_id must be specified.")
+
+    query_data = []
+    for track in jspf["playlist"]["track"]:
+        query_data.append({ "recording_name": track["title"], "artist_name": track["creator"]})
+
+    try:
+        index, recording_id_index = build_track_index()
+        candidate_tracks = lookup_tracks(index, query_data)
+        matched_tracks = []
+        for i, track in enumerate(candidate_tracks):
+            print("QUERY %-50s %-50s" % (query_data[i]['recording_name'][:49], query_data[i]['artist_name'][:49]))
+            print("  HIT %-50s %-50s %d%%\n" % (recording_id_index[track["recording_id"]][1][:49],
+                                                recording_id_index[track["recording_id"]][0][:49], int(100 * track['confidence'])))
+            if track['confidence'] > .7:
+                matched_tracks.append(track)
+
+        create_funkwhale_playlist(user_id, matched_tracks, jspf)
+    except Exception as err:
+        raise InternalServerError(f"Could not build or resolve playlist: {err}")
+
+    return jsonify({ "status": "ok"})
